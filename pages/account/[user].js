76c5546c35175441f6005112702bac06222da88f
Billal <alif.pab120na.12@gmail.com>
2022-03-17 12:30:34 +0600

    clear codes

--- pages/account/[user].js
+++ pages/account/[user].js
@@ -1,10 +1,8 @@
 import React from 'react'
-import {Flex, Avatar, Text} from '@chakra-ui/react'
-import { useSelector } from 'react-redux'
+import {Flex} from '@chakra-ui/react'
 import UserAccount from '../../components/base/UserAccount'
 
 const UserId = () => {
-  const user = useSelector((state) => state.user.user)
 
   return (
     <Flex w={"100%"} mt={10} mr={[0, 100, 150]}>
